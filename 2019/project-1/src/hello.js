ffb6e7d48bea71f6dcbf5dfa8e89655b255184c2
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-07 12:06:31 -0400

    Refactor variable names and add age logging in hello.js and hello.ts

--- 2019/project-1/src/hello.js
+++ 2019/project-1/src/hello.js
@@ -2,3 +2,5 @@ var welcome = 'Welcome to Typescript!';
 console.log(welcome);
 var myDate = '2019-08-27';
 console.log('Date: ', myDate);
+var age = 30;
+console.log('Age: ', age);
--- 2019/project-1/src/hello.ts
+++ 2019/project-1/src/hello.ts
@@ -3,3 +3,6 @@ console.log(welcome);
 
 let myDate = '2019-08-27';
 console.log('Date: ', myDate);
+
+let age = 30;
+console.log('Age: ', age);
