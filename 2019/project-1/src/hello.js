84487b370e5c7aad9352e4f5f094bbe7117c6e33
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-08 12:54:47 -0400

    Refactor variable names and add descriptive logging in hello.js and hello.ts

--- 2019/project-1/src/hello.js
+++ 2019/project-1/src/hello.js
@@ -1,6 +1,6 @@
-var welcome = 'Welcome to Typescript!';
-console.log(welcome);
-var myDate = '2019-08-27';
-console.log('Date: ', myDate);
-var age = 30;
-console.log('Age: ', age);
+var welcomeLanguage = 'Welcome to Typescript!';
+console.log(welcomeLanguage);
+var currentDate = '2019-08-27';
+console.log('Date: ', currentDate);
+var actualAge = 30;
+// console.log('Age: ', actualAge);
--- 2019/project-1/src/hello.ts
+++ 2019/project-1/src/hello.ts
@@ -5,4 +5,4 @@ let currentDate = '2019-08-27';
 console.log('Date: ', currentDate);
 
 let actualAge = 30;
-console.log('Age: ', actualAge);
+// console.log('Age: ', actualAge);
