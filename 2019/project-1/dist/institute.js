a64ceb7a7c80792bbf7522af6953322ef99a29bb
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-12 11:42:36 -0400

    Refactor console log messages and add function for adding numbers

--- 2019/project-1/dist/institute.js
+++ 2019/project-1/dist/institute.js
@@ -2,3 +2,6 @@
 let ourCourses = ['Typescript since scratch V 2.0', 'Angular since scratch'];
 console.log('These are our courses: ', ourCourses);
 const maxStudents = 20;
+function addNumbers(a, b) {
+    return a + b;
+}
--- 2019/project-1/src/institute.ts
+++ 2019/project-1/src/institute.ts
@@ -2,3 +2,7 @@ let ourCourses = ['Typescript since scratch V 2.0', 'Angular since scratch'];
 console.log('These are our courses: ', ourCourses);
 
 const maxStudents = 20;
+
+function addNumbers(a, b) {
+    return a + b;
+}
