53c8aeaa5ff93e5c626f33c645713c43ab020851
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-11-29 21:42:24 -0400

    Add hello.js and institute.js files with initial content; update hello.ts and create test.ts

---
+++ 2019/project-1/dist/institute.js
@@ -0,0 +1,19 @@
+"use strict";
+let ourCourses = ['Typescript since scratch V 2.0', 'Angular since scratch'];
+console.log('These are our courses: ', ourCourses);
+const maxStudents = 20;
+function addNumbers(a, b) {
+    return a + b;
+}
+const myResult = addNumbers(2, 3);
+console.log(myResult);
+// Literal template strings
+console.log(`The number of students is: ${maxStudents}`);
+let myCode = `
+    <button>Hello</button>
+    <div></div>
+`;
+console.log('My Code: ', myCode);
+let course = 'Typescript';
+let waving = `Welcome to the course of ${course}!`;
+console.log(waving);
